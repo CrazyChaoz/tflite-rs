a70f506f1f278a88425976eebd541335d17835b0
Yotaro Kubo <yotaro@google.com>
2021-08-05 13:45:00 +0900

    Updated Tensorflow and dependency to v2.6.0-rc2.
    This commit also updates bindings for flatbuffer schema following the
    latest definition (schema_v3b).

--- build.rs
+++ build.rs
@@ -70,31 +70,15 @@ fn prepare_tensorflow_library() {
         // so a cached version that doesn't match expectations isn't used
         let binary_changing_features = binary_changing_features();
         let tf_lib_name =
-            Path::new(&out_dir).join(format!("libtensorflow-lite{binary_changing_features}.a"));
+            Path::new(&out_dir).join(format!("libtensorflow-lite{}.a", binary_changing_features,));
         let os = env::var("CARGO_CFG_TARGET_OS").expect("Unable to get TARGET_OS");
         if !tf_lib_name.exists() {
             println!("Building tflite");
             let start = Instant::now();
             let mut make = std::process::Command::new("make");
             if let Ok(prefix) = env::var("TARGET_TOOLCHAIN_PREFIX") {
-                make.arg(format!("TARGET_TOOLCHAIN_PREFIX={prefix}"));
-            } else {
-                let target_triple = env::var("TARGET").unwrap();
-                let host_triple = env::var("HOST").unwrap();
-                let kind = if host_triple == target_triple { "HOST" } else { "TARGET" };
-                let target_u = target_triple.replace('-', "_");
-                for name in ["CC", "CXX", "AR", "CFLAGS", "CXXFLAGS", "ARFLAGS"] {
-                    if let Ok(value) = env::var(&format!("{name}_{target_triple}"))
-                        .or_else(|_| env::var(format!("{name}_{target_u}")))
-                        .or_else(|_| env::var(format!("{kind}_{name}")))
-                        .or_else(|_| env::var(name))
-                    {
-                        make.arg(format!("{name}={value}"));
-                        println!("inherited: {name}={value}")
-                    }
-                }
-            }
-
+                make.arg(format!("TARGET_TOOLCHAIN_PREFIX={}", prefix));
+            };
             // Use cargo's cross-compilation information while building tensorflow
             // Now that tensorflow has an aarch64_makefile.inc use theirs
             let target = if &arch == "aarch64" { &arch } else { &os };
@@ -114,43 +98,38 @@ fn prepare_tensorflow_library() {
 
             let make_dir = tflite.parent().unwrap();
 
-            // allow parallelism to be overridden...
-            let num_jobs = env::var("TFLITE_RS_MAKE_PARALLELISM").ok().or_else(|| {
-                // but prefer jobserver if not explicitly given
-                if !env::var("MAKEFLAGS").unwrap_or_default().contains("--jobserver") {
-                    env::var("NUM_JOBS").ok()
-                } else {
-                    None
-                }
-            });
-            if let Some(num_jobs) = num_jobs {
-                make.arg("-j").arg(num_jobs);
-            }
-
-            make.arg("BUILD_WITH_NNAPI=false").arg("-f").arg("tensorflow/lite/tools/make/Makefile");
+            make.arg("-j")
+                // allow parallelism to be overridden
+                .arg(
+                    env::var("TFLITE_RS_MAKE_PARALLELISM").unwrap_or_else(|_| {
+                        env::var("NUM_JOBS").unwrap_or_else(|_| "1".to_string())
+                    }),
+                )
+                .arg("BUILD_WITH_NNAPI=false")
+                .arg("-f")
+                .arg("tensorflow/lite/tools/make/Makefile");
 
             for (make_var, default) in &[
                 ("TARGET", Some(target.as_str())),
                 ("TARGET_ARCH", Some(arch.as_str())),
                 ("TARGET_TOOLCHAIN_PREFIX", None),
                 ("EXTRA_CFLAGS", None),
-                ("EXTRA_CXXFLAGS", None),
             ] {
-                let env_var = format!("TFLITE_RS_MAKE_{make_var}");
-                println!("cargo:rerun-if-env-changed={env_var}");
+                let env_var = format!("TFLITE_RS_MAKE_{}", make_var);
+                println!("cargo:rerun-if-env-changed={}", env_var);
 
                 match env::var(&env_var) {
                     Ok(result) => {
-                        make.arg(format!("{make_var}={result}"));
+                        make.arg(format!("{}={}", make_var, result));
                     }
                     Err(VarError::NotPresent) => {
                         // Try and set some reasonable default values
                         if let Some(result) = default {
-                            make.arg(format!("{make_var}={result}"));
+                            make.arg(format!("{}={}", make_var, result));
                         }
                     }
                     Err(VarError::NotUnicode(_)) => {
-                        panic!("Provided a non-unicode value for {env_var}")
+                        panic!("Provided a non-unicode value for {}", env_var)
                     }
                 }
             }
@@ -162,7 +141,7 @@ fn prepare_tensorflow_library() {
                 make.arg("micro");
             }
             make.current_dir(make_dir);
-            eprintln!("make command = {make:?} in dir  {make_dir:?}");
+            eprintln!("make command = {:?} in dir  {:?}", make, make_dir);
             if !make.status().expect("failed to run make command").success() {
                 panic!("Failed to build tensorflow");
             }
@@ -173,14 +152,14 @@ fn prepare_tensorflow_library() {
                 .filter_map(|de| Some(de.ok()?.path().join("lib/libtensorflow-lite.a")))
                 .find(|p| p.exists())
                 .expect("Unable to find libtensorflow-lite.a");
-            std::fs::copy(library, &tf_lib_name).unwrap_or_else(|_| {
-                panic!("Unable to copy libtensorflow-lite.a to {}", tf_lib_name.display())
+            std::fs::copy(&library, &tf_lib_name).unwrap_or_else(|_| {
+                panic!("{}", format!("Unable to copy libtensorflow-lite.a to {}", tf_lib_name.display()))
             });
 
             println!("Building tflite from source took {:?}", start.elapsed());
         }
-        println!("cargo:rustc-link-search=native={out_dir}");
-        println!("cargo:rustc-link-lib=static=tensorflow-lite{binary_changing_features}");
+        println!("cargo:rustc-link-search=native={}", out_dir);
+        println!("cargo:rustc-link-lib=static=tensorflow-lite{}", binary_changing_features);
     }
     #[cfg(not(feature = "build"))]
     {
@@ -212,7 +191,7 @@ fn import_tflite_types() {
     let submodules = submodules();
     let submodules_str = submodules.to_string_lossy();
     let bindings = Builder::default()
-        .allowlist_recursively(true)
+        .whitelist_recursively(true)
         .prepend_enum_name(false)
         .impl_debug(true)
         .with_codegen_config(CodegenConfig::TYPES)
@@ -221,40 +200,39 @@ fn import_tflite_types() {
         .derive_default(true)
         .size_t_is_usize(true)
         // for model APIs
-        .allowlist_type("tflite::ModelT")
-        .allowlist_type(".+OptionsT")
-        .blocklist_type(".+_TableType")
+        .whitelist_type("tflite::ModelT")
+        .whitelist_type(".+OptionsT")
+        .blacklist_type(".+_TableType")
         // for interpreter
-        .allowlist_type("tflite::FlatBufferModel")
+        .whitelist_type("tflite::FlatBufferModel")
         .opaque_type("tflite::FlatBufferModel")
-        .allowlist_type("tflite::InterpreterBuilder")
+        .whitelist_type("tflite::InterpreterBuilder")
         .opaque_type("tflite::InterpreterBuilder")
-        .allowlist_type("tflite::Interpreter")
+        .whitelist_type("tflite::Interpreter")
         .opaque_type("tflite::Interpreter")
-        .allowlist_type("tflite::ops::builtin::BuiltinOpResolver")
+        .whitelist_type("tflite::ops::builtin::BuiltinOpResolver")
         .opaque_type("tflite::ops::builtin::BuiltinOpResolver")
-        .allowlist_type("tflite::OpResolver")
+        .whitelist_type("tflite::OpResolver")
         .opaque_type("tflite::OpResolver")
-        .allowlist_type("TfLiteTensor")
+        .whitelist_type("TfLiteTensor")
         .opaque_type("std::string")
+        .opaque_type("std::basic_string.*")
         .opaque_type("flatbuffers::NativeTable")
-        .blocklist_type("std")
-        .blocklist_type("tflite::Interpreter_TfLiteDelegatePtr")
-        .blocklist_type("tflite::Interpreter_State")
+        .blacklist_type("std")
+        .blacklist_type("tflite::Interpreter_TfLiteDelegatePtr")
+        .blacklist_type("tflite::Interpreter_State")
         .default_enum_style(EnumVariation::Rust { non_exhaustive: false })
         .derive_partialeq(true)
         .derive_eq(true)
         .header("csrc/tflite_wrapper.hpp")
-        .clang_arg(format!("-I{submodules_str}/tensorflow"))
-        .clang_arg(format!("-I{submodules_str}/downloads/flatbuffers/include"))
-        .clang_arg("-DGEMMLOWP_ALLOW_SLOW_SCALAR_FALLBACK")
+        .clang_arg(format!("-I{}/tensorflow", submodules_str))
+        .clang_arg(format!("-I{}/downloads/flatbuffers/include", submodules_str))
         .clang_arg("-DFLATBUFFERS_POLYMORPHIC_NATIVETABLE")
         .clang_arg("-x")
         .clang_arg("c++")
         .clang_arg("-std=c++11")
         // required to get cross compilation for aarch64 to work because of an issue in flatbuffers
-        .clang_arg("-fms-extensions")
-        .no_copy("_Tp");
+        .clang_arg("-fms-extensions");
 
     let bindings = bindings.generate().expect("Unable to generate bindings");
 
@@ -272,7 +250,6 @@ fn build_inline_cpp() {
         .flag("-fPIC")
         .flag("-std=c++14")
         .flag("-Wno-sign-compare")
-        .define("GEMMLOWP_ALLOW_SLOW_SCALAR_FALLBACK", None)
         .define("FLATBUFFERS_POLYMORPHIC_NATIVETABLE", None)
         .debug(true)
         .opt_level(if cfg!(debug_assertions) { 0 } else { 2 })
@@ -284,11 +261,11 @@ fn import_stl_types() {
 
     let bindings = Builder::default()
         .enable_cxx_namespaces()
-        .allowlist_type("std::string")
+        .whitelist_type("std::string")
         .opaque_type("std::string")
-        .allowlist_type("rust::.+")
+        .whitelist_type("rust::.+")
         .opaque_type("rust::.+")
-        .blocklist_type("std")
+        .blacklist_type("std")
         .header("csrc/stl_wrapper.hpp")
         .layout_tests(false)
         .derive_partialeq(true)
@@ -297,7 +274,7 @@ fn import_stl_types() {
         .clang_arg("c++")
         .clang_arg("-std=c++14")
         .clang_arg("-fms-extensions")
-        .formatter(Formatter::Rustfmt)
+        .rustfmt_bindings(false)
         .generate()
         .expect("Unable to generate STL bindings");
 
@@ -313,7 +290,6 @@ fn generate_memory_impl() -> Result<(), Box<dyn std::error::Error>> {
     writeln!(
         &mut file,
         r#"
-#![allow(clippy::transmute_num_to_bytes)]
 use std::{{fmt, mem}};
 use std::ops::{{Deref, DerefMut}};
 
@@ -337,6 +313,8 @@ use crate::model::stl::memory::UniquePtr;
         ("QuantizationParametersT", "crate::model::QuantizationParametersT"),
         ("ModelT", "crate::model::ModelT"),
         ("MetadataT", "crate::model::MetadataT"),
+        ("TensorMapT", "crate::model::TensorMapT"),
+        ("SignatureDefT", "crate::model::SignatureDefT"),
     ];
 
     for (cpp_type, rust_type) in memory_types {
@@ -352,7 +330,6 @@ fn generate_vector_impl() -> Result<(), Box<dyn std::error::Error>> {
     writeln!(
         &mut file,
         r#"
-#![allow(clippy::transmute_num_to_bytes)]
 use std::{{fmt, mem, slice}};
 use std::ops::{{Deref, DerefMut, Index, IndexMut}};
 
@@ -403,6 +380,8 @@ cpp! {{{{
         ("std::unique_ptr<SubGraphT>", "UniquePtr<crate::model::SubGraphT>"),
         ("std::unique_ptr<BufferT>", "UniquePtr<crate::model::BufferT>"),
         ("std::unique_ptr<MetadataT>", "UniquePtr<crate::model::MetadataT>"),
+        ("std::unique_ptr<SignatureDefT>", "UniquePtr<crate::model::SignatureDefT>"),
+        ("std::unique_ptr<TensorMapT>", "UniquePtr<crate::model::TensorMapT>"),
     ];
 
     for (cpp_type, rust_type) in vector_types {
--- src/model/stl/memory_impl.rs
+++ src/model/stl/memory_impl.rs
@@ -1,6 +1,6 @@
-#![allow(clippy::transmute_num_to_bytes)]
-use std::ops::{Deref, DerefMut};
+
 use std::{fmt, mem};
+use std::ops::{Deref, DerefMut};
 
 use crate::model::stl::memory::UniquePtr;
 
@@ -26,7 +26,7 @@ impl Deref for UniquePtr<crate::model::OperatorCodeT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<OperatorCodeT>*"] -> *const crate::model::OperatorCodeT as "const OperatorCodeT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -39,7 +39,7 @@ impl DerefMut for UniquePtr<crate::model::OperatorCodeT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<OperatorCodeT>*"] -> *mut crate::model::OperatorCodeT as "OperatorCodeT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -47,11 +47,13 @@ impl DerefMut for UniquePtr<crate::model::OperatorCodeT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::OperatorCodeT> {
+impl fmt::Debug for UniquePtr<crate::model::OperatorCodeT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
 
 #[allow(deprecated)]
 impl Default for UniquePtr<crate::model::TensorT> {
@@ -75,7 +77,7 @@ impl Deref for UniquePtr<crate::model::TensorT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<TensorT>*"] -> *const crate::model::TensorT as "const TensorT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -88,7 +90,7 @@ impl DerefMut for UniquePtr<crate::model::TensorT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<TensorT>*"] -> *mut crate::model::TensorT as "TensorT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -96,11 +98,13 @@ impl DerefMut for UniquePtr<crate::model::TensorT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::TensorT> {
+impl fmt::Debug for UniquePtr<crate::model::TensorT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
 
 #[allow(deprecated)]
 impl Default for UniquePtr<crate::model::OperatorT> {
@@ -124,7 +128,7 @@ impl Deref for UniquePtr<crate::model::OperatorT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<OperatorT>*"] -> *const crate::model::OperatorT as "const OperatorT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -137,7 +141,7 @@ impl DerefMut for UniquePtr<crate::model::OperatorT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<OperatorT>*"] -> *mut crate::model::OperatorT as "OperatorT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -145,11 +149,13 @@ impl DerefMut for UniquePtr<crate::model::OperatorT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::OperatorT> {
+impl fmt::Debug for UniquePtr<crate::model::OperatorT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
 
 #[allow(deprecated)]
 impl Default for UniquePtr<crate::model::SubGraphT> {
@@ -173,7 +179,7 @@ impl Deref for UniquePtr<crate::model::SubGraphT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<SubGraphT>*"] -> *const crate::model::SubGraphT as "const SubGraphT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -186,7 +192,7 @@ impl DerefMut for UniquePtr<crate::model::SubGraphT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<SubGraphT>*"] -> *mut crate::model::SubGraphT as "SubGraphT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -194,11 +200,13 @@ impl DerefMut for UniquePtr<crate::model::SubGraphT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::SubGraphT> {
+impl fmt::Debug for UniquePtr<crate::model::SubGraphT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
 
 #[allow(deprecated)]
 impl Default for UniquePtr<crate::model::BufferT> {
@@ -222,7 +230,7 @@ impl Deref for UniquePtr<crate::model::BufferT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<BufferT>*"] -> *const crate::model::BufferT as "const BufferT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -235,7 +243,7 @@ impl DerefMut for UniquePtr<crate::model::BufferT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<BufferT>*"] -> *mut crate::model::BufferT as "BufferT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -243,11 +251,13 @@ impl DerefMut for UniquePtr<crate::model::BufferT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::BufferT> {
+impl fmt::Debug for UniquePtr<crate::model::BufferT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
 
 #[allow(deprecated)]
 impl Default for UniquePtr<crate::model::QuantizationParametersT> {
@@ -271,7 +281,7 @@ impl Deref for UniquePtr<crate::model::QuantizationParametersT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<QuantizationParametersT>*"] -> *const crate::model::QuantizationParametersT as "const QuantizationParametersT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -284,7 +294,7 @@ impl DerefMut for UniquePtr<crate::model::QuantizationParametersT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<QuantizationParametersT>*"] -> *mut crate::model::QuantizationParametersT as "QuantizationParametersT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -292,11 +302,13 @@ impl DerefMut for UniquePtr<crate::model::QuantizationParametersT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::QuantizationParametersT> {
+impl fmt::Debug for UniquePtr<crate::model::QuantizationParametersT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
 
 #[allow(deprecated)]
 impl Default for UniquePtr<crate::model::ModelT> {
@@ -320,7 +332,7 @@ impl Deref for UniquePtr<crate::model::ModelT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<ModelT>*"] -> *const crate::model::ModelT as "const ModelT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -333,7 +345,7 @@ impl DerefMut for UniquePtr<crate::model::ModelT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<ModelT>*"] -> *mut crate::model::ModelT as "ModelT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -341,11 +353,13 @@ impl DerefMut for UniquePtr<crate::model::ModelT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::ModelT> {
+impl fmt::Debug for UniquePtr<crate::model::ModelT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
 
 #[allow(deprecated)]
 impl Default for UniquePtr<crate::model::MetadataT> {
@@ -369,7 +383,7 @@ impl Deref for UniquePtr<crate::model::MetadataT> {
         unsafe {
             let ptr = cpp!([self as "const std::unique_ptr<MetadataT>*"] -> *const crate::model::MetadataT as "const MetadataT*" {
                 return self->get();
-            });
+            }) as *const Self::Target;
 
             ptr.as_ref().unwrap()
         }
@@ -382,7 +396,7 @@ impl DerefMut for UniquePtr<crate::model::MetadataT> {
         unsafe {
             let ptr = cpp!([self as "std::unique_ptr<MetadataT>*"] -> *mut crate::model::MetadataT as "MetadataT*" {
                 return self->get();
-            });
+            }) as *mut Self::Target;
 
             ptr.as_mut().unwrap()
         }
@@ -390,8 +404,113 @@ impl DerefMut for UniquePtr<crate::model::MetadataT> {
 }
 
 #[allow(deprecated)]
-impl fmt::Debug for UniquePtr<crate::model::MetadataT> {
+impl fmt::Debug for UniquePtr<crate::model::MetadataT>
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "({:?})", self.deref())
     }
 }
+
+
+#[allow(deprecated)]
+impl Default for UniquePtr<crate::model::TensorMapT> {
+    fn default() -> Self {
+        let mut this: Self = unsafe { mem::zeroed() };
+        let this_ref = &mut this;
+        unsafe {
+            cpp!([this_ref as "std::unique_ptr<TensorMapT>*"] {
+                new (this_ref) std::unique_ptr<TensorMapT>(new TensorMapT);
+            })
+        }
+        this
+    }
+}
+
+#[allow(deprecated)]
+impl Deref for UniquePtr<crate::model::TensorMapT> {
+    type Target = crate::model::TensorMapT;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            let ptr = cpp!([self as "const std::unique_ptr<TensorMapT>*"] -> *const crate::model::TensorMapT as "const TensorMapT*" {
+                return self->get();
+            }) as *const Self::Target;
+
+            ptr.as_ref().unwrap()
+        }
+    }
+}
+
+#[allow(deprecated)]
+impl DerefMut for UniquePtr<crate::model::TensorMapT> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            let ptr = cpp!([self as "std::unique_ptr<TensorMapT>*"] -> *mut crate::model::TensorMapT as "TensorMapT*" {
+                return self->get();
+            }) as *mut Self::Target;
+
+            ptr.as_mut().unwrap()
+        }
+    }
+}
+
+#[allow(deprecated)]
+impl fmt::Debug for UniquePtr<crate::model::TensorMapT>
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({:?})", self.deref())
+    }
+}
+
+
+#[allow(deprecated)]
+impl Default for UniquePtr<crate::model::SignatureDefT> {
+    fn default() -> Self {
+        let mut this: Self = unsafe { mem::zeroed() };
+        let this_ref = &mut this;
+        unsafe {
+            cpp!([this_ref as "std::unique_ptr<SignatureDefT>*"] {
+                new (this_ref) std::unique_ptr<SignatureDefT>(new SignatureDefT);
+            })
+        }
+        this
+    }
+}
+
+#[allow(deprecated)]
+impl Deref for UniquePtr<crate::model::SignatureDefT> {
+    type Target = crate::model::SignatureDefT;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            let ptr = cpp!([self as "const std::unique_ptr<SignatureDefT>*"] -> *const crate::model::SignatureDefT as "const SignatureDefT*" {
+                return self->get();
+            }) as *const Self::Target;
+
+            ptr.as_ref().unwrap()
+        }
+    }
+}
+
+#[allow(deprecated)]
+impl DerefMut for UniquePtr<crate::model::SignatureDefT> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe {
+            let ptr = cpp!([self as "std::unique_ptr<SignatureDefT>*"] -> *mut crate::model::SignatureDefT as "SignatureDefT*" {
+                return self->get();
+            }) as *mut Self::Target;
+
+            ptr.as_mut().unwrap()
+        }
+    }
+}
+
+#[allow(deprecated)]
+impl fmt::Debug for UniquePtr<crate::model::SignatureDefT>
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({:?})", self.deref())
+    }
+}
+
+

958e3d54459a56bd8d176d835e019a290b264204
CrazyChaoz <kempinger@ins.jku.at>
2024-11-25 20:40:25 +0100

    changed nix flake for gh workflow

--- flake.nix
+++ flake.nix
@@ -4,14 +4,38 @@
     utils = {
       url = "github:numtide/flake-utils";
     };
+
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, crane }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = (crane.mkLib pkgs).buildPackage {
+          src = ./.;
+          strictDeps = true;
+          doCheck = true;
+
+          TFLITE_X86_64_LIB_DIR = "${pkgs.tensorflow-lite}/lib";
+          TFLITE_LIB_DIR = "${pkgs.tensorflow-lite}/lib";
+
+          buildInputs = with pkgs; [
+            tensorflow-lite
+            vtk
+          ];
+          nativeBuildInputs = with pkgs; [
+            clang
+            pkg-config
+            perl
+            rustPlatform.bindgenHook
+            cmake
+            libclang
+          ];
+        };
+
         devShell = pkgs.mkShell {
           TFLITE_X86_64_LIB_DIR = "${pkgs.tensorflow-lite}/lib";
           TFLITE_LIB_DIR = "${pkgs.tensorflow-lite}/lib";
